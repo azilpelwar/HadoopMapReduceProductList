1e57dac5c90f0d4a53292f907b5a27ff343698a3
Ankur Zilpelwar <ankur.zilpelwar@gmail.com>
2014-05-18 10:00:33 +0400

    Add Reducer code

--- mapper.py
+++ mapper.py
@@ -2,6 +2,10 @@
 
 # Format of each line is:
 # date\ttime\tstore name\titem description\tcost\tmethod of payment
+#
+# We want elements 2 (store name) and 4 (cost)
+# We need to write them out to standard output, separated by a tab
+
 import sys
 
 def mapper():
---
+++ reducer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+# We are using the Hadoop streaming for communicating to the Hadoop engine
+# Hadoop streaming allows Hadoop to communicate with any language 
+# Loop around the data
+# It will be in the format key\tval
+# Where key is the store name, val is the sale amount
+#
+# All the sales for a particular store will be presented,
+# then the key will change and we'll be dealing with the next store
+
+import sys
+def reducer():
+	salesTotal=0
+	oldKey=None
+	for line in sys.stdin:
+		data=line.strip.split("\t")
+		if len(data) != 2:
+			continue
+		thisKey, thisSale = data
+		if oldKey and oldKey!=thiKey:
+			print "{0}\t{1}".format(oldKey,salesTotal)
+			salesTotal=0
+		oldKey=thisKey
+		salesTotal += float(thisSale)
+	if oldKey!=None:
+		print "{0}\t{1}".format(oldKey,salesTotal)
