1bb5204a2e9d9ddecba94dbfe90b41e9c62dc655
Ankur Zilpelwar <ankur.zilpelwar@gmail.com>
2014-05-18 10:25:56 +0400

    Correct reducer Errors. Add the commands for simulation of Hadoop mapReduce job on linux terminal using linux commnds

--- reducer.py
+++ reducer.py
@@ -10,19 +10,19 @@
 # then the key will change and we'll be dealing with the next store
 
 import sys
-def reducer():
-	salesTotal=0
-	oldKey=None
-	for line in sys.stdin:
-		data=line.strip.split("\t")
-		if len(data) != 2:
-			continue
-		thisKey, thisSale = data
-		if oldKey and oldKey!=thiKey:
-			print "{0}\t{1}".format(oldKey,salesTotal)
-			salesTotal=0
-		oldKey=thisKey
-		salesTotal += float(thisSale)
-	#Finally print result for Last store from the DB
-	if oldKey!=None:
+
+salesTotal=0
+oldKey=None
+for line in sys.stdin:
+	data = line.strip().split("\t")
+	if len(data) != 2:
+		continue
+	thisKey, thisSale = data
+	if oldKey and oldKey!=thisKey:
 		print "{0}\t{1}".format(oldKey,salesTotal)
+		salesTotal=0
+	oldKey=thisKey
+	salesTotal += float(thisSale)
+#Finally print result for Last store from the DB
+if oldKey!=None:
+	print "{0}\t{1}".format(oldKey,salesTotal)
